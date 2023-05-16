f90a234f7855db4e39a33eee6ab8709b3d85c738
INGENIO <ingeniokhalif@gmail.com>
2023-05-16 17:53:57 +0100

    images handled

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../config/db");
+const fs = require("fs");
 
 const getAllArticlesHandler = asyncHandler(async (req, res) => {
   await db.query(
@@ -15,9 +16,31 @@ const getAllArticlesHandler = asyncHandler(async (req, res) => {
 
 // Create an article
 const createArticleHandler = asyncHandler(async (req, res, next) => {
-  // const image_art = req.file;
-  console.log(req.res);
-  // res.status(201).json(image_art);
+  const { id_cat, nom_art, desc_art, prix_art } = req.body;
+
+  // Gets the extension of the file
+  // sets the new name
+  // and rename the file and its path
+  const ext = req.file.mimetype.split("/")[1];
+  const newName = req.file.filename + "." + ext;
+  fs.rename(
+    "./public/uploads/" + req.file.filename,
+    "./public/uploads/" + newName,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  const newPath = req.file.path + "." + ext;
+
+  await db.query(
+    {
+      sql: "INSERT INTO article(id_cat, nom_art, desc_art, prix_art, image_art) VALUES(?,?,?,?,?)",
+    },
+    [id_cat, nom_art, desc_art, prix_art, newPath], (errors, result) =>{
+      if(errors) throw errors;
+      res.status(201).json({ insertedId: result.insertId, message: "Article inserted successfully" });
+    }
+  );
 });
 
 const getArticleHandler = asyncHandler(async (req, res) => {
