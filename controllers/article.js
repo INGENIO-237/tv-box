e34e673b7e4c63459f22d9355bdba17f188a3356
INGENIO <ingeniokhalif@gmail.com>
2023-05-16 09:03:36 +0100

    milestone 1

--- controllers/article.js
+++ controllers/article.js
@@ -13,12 +13,16 @@ const getAllArticlesHandler = asyncHandler(async (req, res) => {
   );
 });
 
-const createArticleHandler = asyncHandler(async (req, res) => {
-  const { id_cat, nom_art, desc_art, prix_art, image_art } = req.body;
+// Create an article
+const createArticleHandler = asyncHandler(async (req, res, next) => {
+  const { id_cat, nom_art, desc_art, prix_art } = req.body;
+  const image_art = req.file;
   if (!id_cat || !nom_art || !desc_art || !prix_art || !image_art) {
     res.status(400).json({ message: "All fields are mandatory !" });
+  }else{
+    res.status(201).json(req.file.filename);
+    next();
   }
-  res.status(201).json({ message: "Create an article" });
 });
 
 const getArticleHandler = asyncHandler(async (req, res) => {
--- routes/article.js
+++ routes/article.js
@@ -6,6 +6,10 @@ const {
   deleteArticleHandler,
 } = require("../controllers/article");
 
+// Package in charge of handling images
+const multer = require("multer");
+const upload = multer({ dest: '../public/uploads', storage: multer.diskStorage() });
+
 const router = require("express").Router();
 
 // @desc Get all articles
@@ -16,7 +20,7 @@ router.get("/", getAllArticlesHandler);
 // @desc Create an article
 // @route POST /api/{version}/articles
 // public
-router.post("/", createArticleHandler);
+router.post("/", upload.single('image_art'), createArticleHandler);
 
 // @desc Get single article
 // @route GET /api/{version}/articles/:id
