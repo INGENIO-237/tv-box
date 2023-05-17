9002071c5106c938981c6c160369e8c9f8af8497
INGENIO <ingeniokhalif@gmail.com>
2023-05-17 13:27:14 +0100

    milestone 2

--- controllers/user.js
+++ controllers/user.js
@@ -43,18 +43,75 @@ const getUsersByRoleHandler = asyncHandler(async (req, res) => {
       [req.params.role],
       (errors, result) => {
         if (errors) throw errors;
-          res.status(200).json(result);
+        res.status(200).json(result);
       }
     );
   }
 });
 
 const updateUserHandler = asyncHandler(async (req, res) => {
-  res.status(204).json({ message: `Update user with id : ${req.params.id}` });
+  if (req.params.id) {
+    await db.query(
+      {
+        sql: "SELECT * FROM utilisateur ut, role rol WHERE ut.id_role = rol.id_role AND ut.id_usr = ?",
+      },
+      [req.params.id],
+      (errors, result) => {
+        if (errors) throw errors;
+        if (result.length == 0) {
+          res
+            .status(404)
+            .json({ message: `User with id ${req.params.id} does not exist` });
+        } else {
+          const { id_role, nom_usr, prenom_usr, phone_usr } = req.body;
+          if (!id_role || !nom_usr || !prenom_usr || !phone_usr) {
+            res.status(400).json({ message: "All fields are mandatory" });
+          } else {
+            db.query(
+              {
+                sql: "UPDATE utilisateur SET id_role = ?, nom_usr = ?, prenom_usr = ?, phone_usr = ? WHERE id_usr = ?",
+              },
+              [id_role, nom_usr, prenom_usr, phone_usr, req.params.id],
+              (errors, result) => {
+                if (errors) throw errors;
+                res.status(200).json({ message: "User updated successfully" });
+              }
+            );
+          }
+        }
+      }
+    );
+  }
 });
 
 const deleteUserHandler = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete user with id : ${req.params.id}` });
+  if (req.params.id) {
+    await db.query(
+      {
+        sql: "SELECT * FROM utilisateur ut, role rol WHERE ut.id_role = rol.id_role AND ut.id_usr = ?",
+      },
+      [req.params.id],
+      (errors, result) => {
+        if (errors) throw errors;
+        if (result.length == 0) {
+          res
+            .status(404)
+            .json({ message: `User with id ${req.params.id} does not exist` });
+        } else {
+          db.query(
+            {
+              sql: "DELETE FROM utilisateur WHERE id_usr = ?",
+            },
+            [req.params.id],
+            (errors, result) => {
+              if (errors) throw errors;
+              res.status(200).json({ message: "User deleted successfully" });
+            }
+          );
+        }
+      }
+    );
+  }
 });
 
 const getUserGainsHandler = asyncHandler(async (req, res) => {
